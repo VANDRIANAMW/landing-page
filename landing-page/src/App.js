2514632b9db2586dd5ff5a2576c47ef937f80b41
Camilo Oñate Moraga <caonatem@gmail.com>
2019-05-28 21:03:30 -0400

    listopagefeatures

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
 import Jumbotron from './components/jumbotron';
+import Paginas from './components/pagefeatures';
 
 function App() {
   return (
     <div className="App">
-       <Navbar/>
-       <Jumbotron/>
-      <Footer />
+       <Navbar />
+       <Jumbotron />
+       <Footer />
+       <Paginas />
     </div>
-  );
+  )
 }
 
 export default App;
